1d118a5f6c86072b87d32e2819fd0664ee51b92c
gummachado <carinadidomenico@gmail.com>
2018-10-31 21:53:00 -0300

    javaaula

---
+++ script.js
@@ -0,0 +1,64 @@
+// let maos = prompt('quantas maos voce tem');
+
+// let pes = prompt('Quantos pes voce tem?');
+
+// let total =  parseFloat(pes) + parseFloat(maos);
+
+// alert( `Voce tem no total : ${total} membros `);
+
+
+// Ingredientes=  200g de açúcar
+// 3000g de farinha de trigo
+// 150g de margarina
+// 3 ovos
+// 150g de leite
+// 100g de fermento em pó
+
+// aula 1
+
+// console.log("FOI");
+// let texto = "texto";
+// let numero = 1;
+// let booleano = true;
+
+// let nome = prompt("Qual o seu nome");
+// let valor = prompt ("qual o valor do seu emprestimo");
+// // let pretendePagar = confirm ("Voce pretende pagar o seu emprestimo?");
+
+// alert (`Ola ${nome}`);
+// alert (`Voce gostaria de emprestar rs${valor},certo?`);
+
+// let pretendePagar = confirm("Voce pretende pagar o seu emprestimo?");
+// alert("Ok! Obrigado por nos visitar!");
+
+
+// let lista = [1, 2, 3, 4, 5," Ola Gustavo"];
+let joao = {
+nome: "Joao",
+idade: "20",
+namorada: "vamo q esse ano ainda vai",
+gatos: [
+   { nome:"claudio",
+    raca:"gatao"}
+    ,
+
+    {nome:"gustavo",
+    raca:"gatissimo"} 
+    ,
+]
+};
+let maria = {
+    nome:"maria",
+    idade:"20",
+    namorado:joao,
+    cachorros:[
+        {nome:"luke",
+        raca:"pastor alemao"}
+        ,
+
+        {nome:"fred",
+         raca:"poodle"}
+        ,
+    ]
+};
+
